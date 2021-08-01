40758507135ebcf17592d69d077e9314174996e4
LEE <dulfla_5@naver.com>
2021-08-01 11:31:11 +0900

    2751

---
+++ src/main/java/Main2751.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main2751 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            numbers.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(numbers);
+        for (int number : numbers) {
+            bw.write(Integer.toString(number));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
