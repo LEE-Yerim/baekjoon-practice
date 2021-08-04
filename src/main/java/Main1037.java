c76cc1ed15dc96a5540db0472df6857c1ed4a3fa
LEE <dulfla_5@naver.com>
2021-08-04 13:18:59 +0900

    1037

---
+++ src/main/java/Main1037.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main1037 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int max = numbers[0];
+        int min = numbers[0];
+        for (int number : numbers) {
+            max = Math.max(max, number);
+            min = Math.min(min, number);
+        }
+        System.out.println(max * min);
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
