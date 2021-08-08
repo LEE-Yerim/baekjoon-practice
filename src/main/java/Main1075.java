06689304aed2dbd54597c0cc0ec282d2312913f7
LEE <dulfla_5@naver.com>
2021-08-08 13:27:32 +0900

    1075

---
+++ src/main/java/Main1075.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1075 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int f = Integer.parseInt(br.readLine());
+        int number = n - n % 100;
+        while (number % f != 0) {
+            number++;
+        }
+        String answer = Integer.toString(number % 100);
+        if (answer.length() == 1) {
+            answer = "0" + answer;
+        }
+        System.out.println(answer);
+    }
+}
