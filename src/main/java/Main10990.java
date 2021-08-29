be220e571f98ae75d6eafa7e19824d612d458356
LEE <dulfla_5@naver.com>
2021-08-29 22:53:19 +0900

    10990

---
+++ src/main/java/Main10990.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main10990 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.println();
+
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < n - j - 1; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int i = 0; i < j * 2 - 1; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
