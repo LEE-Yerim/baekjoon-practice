5e84fe3c709d81ef8f84330a69a1fe26f4894e23
LEE <dulfla_5@naver.com>
2021-09-17 16:20:02 +0900

    7287

---
+++ src/main/java/Main7287.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main7287 {
+    public static void solution() throws IOException {
+        System.out.println(81);
+        System.out.println("dulfla5");
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
