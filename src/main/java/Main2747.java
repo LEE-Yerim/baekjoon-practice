8e5e03824c43f952195de56944393b24e950ace7
LEE <dulfla_5@naver.com>
2021-09-29 12:59:05 +0900

    2747

---
+++ src/main/java/Main2747.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2747 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fib(n));
+    }
+
+    private static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
