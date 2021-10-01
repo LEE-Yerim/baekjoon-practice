58b206d7ca3b544a2474f90dc01c6d71468e363f
LEE <dulfla_5@naver.com>
2021-10-01 16:09:19 +0900

    2609

---
+++ src/main/java/Main2609.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2609 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static void solution() throws IOException {
+        String[] input_numbers = br.readLine().split(" ");
+        int A = Integer.parseInt(input_numbers[0]);
+        int B = Integer.parseInt(input_numbers[1]);
+        int j;
+        for (j = A; j > 0; j--) {
+            if (A % j == 0 && B % j == 0) {
+                break;
+            }
+        }
+        System.out.println(j);
+        System.out.println(A * B / j);
+    }
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
