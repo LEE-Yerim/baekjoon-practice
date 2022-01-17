67baeb28dc40afe13636871ef4c467ffc72d7e12
LEE <dulfla_5@naver.com>
2022-01-18 01:35:56 +0900

    소트인사이드

---
+++ src/main/java/정렬/Main1427.java
@@ -0,0 +1,24 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main1427 {
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void solution() throws IOException {
+        String N = bf.readLine();
+        String[] digits = N.split("");
+        Arrays.sort(digits, Collections.reverseOrder());
+        for (String digit : digits) {
+            System.out.print(digit);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
