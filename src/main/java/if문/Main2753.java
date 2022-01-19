611a3ba75a4eda6dfa26b028eb2beb53d30e774e
LEE <dulfla_5@naver.com>
2022-01-19 22:15:39 +0900

    윤년

---
+++ src/main/java/if문/Main2753.java
@@ -0,0 +1,22 @@
+package if문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2753 {
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void solution() throws IOException {
+        int year = Integer.parseInt(bf.readLine());
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
