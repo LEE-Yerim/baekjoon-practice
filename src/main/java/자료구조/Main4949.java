4010961ec56238a13344d5254b800b58cc7e844a
LEE <dulfla_5@naver.com>
2022-01-21 17:49:36 +0900

    균형잡힌 세상

---
+++ src/main/java/자료구조/Main4949.java
@@ -0,0 +1,50 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main4949 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static void solution() throws IOException {
+        String str = "";
+        while (!(str = br.readLine()).equals(".")) {
+            String[] characters = str.split("");
+            Stack<String> stack = new Stack<>();
+            for (String character : characters) {
+                if (character.equals("(")) {
+                    stack.push("(");
+                }
+                if (character.equals(")")) {
+                    if (!stack.isEmpty() && stack.peek().equals("(")) {
+                        stack.pop();
+                    } else {
+                        stack.push(")");
+                    }
+                }
+                if (character.equals("[")) {
+                    stack.push("[");
+                }
+                if (character.equals("]")) {
+                    if (!stack.isEmpty() && stack.peek().equals("[")) {
+                        stack.pop();
+                    } else {
+                        stack.push("]");
+                    }
+                }
+            }
+            if (stack.isEmpty()) {
+                System.out.println("yes");
+            }
+            if (!stack.isEmpty()) {
+                System.out.println("no");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
