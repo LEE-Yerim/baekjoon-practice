878ce104e46fcdeeb162b36db7b50ec970abe10d
LEE <dulfla_5@naver.com>
2022-01-22 16:36:15 +0900

    시험 성적

---
+++ src/main/java/if문/Main9498.java
@@ -0,0 +1,23 @@
+package if문;
+
+import java.io.*;
+
+public class Main9498 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int score = Integer.parseInt(br.readLine());
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
