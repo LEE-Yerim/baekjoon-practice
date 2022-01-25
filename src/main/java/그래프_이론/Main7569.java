e0c92d5fe3a7a455f4e2e93b2206fa9f6f733321
LEE <dulfla_5@naver.com>
2022-01-25 13:10:45 +0900

    토마토

--- src/main/java/그래프_이론/Main7569.java
+++ src/main/java/그래프_이론/Main7569.java
@@ -1,6 +1,10 @@
+package 그래프_이론;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main7569 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,8 +16,19 @@ public class Main7569 {
     private static int height;
     private static int floor;
     private static int[][][] box;
-    private static boolean[][][] visited;
-    private static int day;
+    private static Queue<Tomato> queue;
+
+    public static class Tomato {
+        int f;
+        int h;
+        int w;
+
+        public Tomato(int f, int h, int w) {
+            this.f = f;
+            this.h = h;
+            this.w = w;
+        }
+    }
 
     public static void solution() throws IOException {
         String input = br.readLine();
@@ -22,51 +37,41 @@ public class Main7569 {
         floor = Integer.parseInt(input.split(" ")[2]);
 
         box = new int[floor][height][width];
+        queue = new LinkedList<>();
 
         for (int i = 0; i < floor; i++) {
             for (int j = 0; j < height; j++) {
                 String[] row = br.readLine().split(" ");
                 for (int k = 0; k < width; k++) {
                     box[i][j][k] = Integer.parseInt(row[k]);
-                }
-            }
-        }
-
-        visited = new boolean[floor][height][width];
-        day = 0;
-        for (int i = 0; i < floor; i++) {
-            for (int j = 0; j < height; j++) {
-                for (int k = 0; k < width; k++) {
-                    if (!visited[i][j][k] && box[i][j][k] == 1) {
-                        ripen(i, j, k);
+                    if (box[i][j][k] == 1) {
+                        queue.add(new Tomato(i, j, k));
                     }
                 }
             }
         }
 
-        for (int i = 0; i < floor; i++) {
-            for (int j = 0; j < height; j++) {
-                for (int k = 0; k < width; k++) {
-                    System.out.print(box[i][j][k] + " ");
-                }
-                System.out.println();
-            }
-        }
+        bfs();
+
         System.out.println(result());
     }
 
-    public static void ripen(int this_floor, int this_height, int this_width) {
-        visited[this_floor][this_height][this_width] = true;
-        day++;
-        for (int i = 0; i < 6; i++) {
-            int next_floor = this_floor + df[i];
-            int next_height = this_height + dh[i];
-            int next_width = this_width + dw[i];
+    public static void bfs() {
+        while (!queue.isEmpty()) {
+            Tomato tomato = queue.remove();
+            int this_floor = tomato.f;
+            int this_height = tomato.h;
+            int this_width = tomato.w;
+            for (int i = 0; i < 6; i++) {
+                int next_floor = this_floor + df[i];
+                int next_height = this_height + dh[i];
+                int next_width = this_width + dw[i];
 
-            if (next_floor >= 0 && next_floor < floor && next_height >= 0 && next_height < height && next_width >= 0 && next_width < width) {
-                if (box[next_floor][next_height][next_width] == 0 && !visited[next_floor][next_height][next_width]) {
-                    box[next_floor][next_height][next_width] = 1;
-                    ripen(next_floor, next_height, next_width);
+                if (next_floor >= 0 && next_floor < floor && next_height >= 0 && next_height < height && next_width >= 0 && next_width < width) {
+                    if (box[next_floor][next_height][next_width] == 0) {
+                        queue.add(new Tomato(next_floor, next_height, next_width));
+                        box[next_floor][next_height][next_width] = box[this_floor][this_height][this_width] + 1;
+                    }
                 }
             }
         }
@@ -82,7 +87,15 @@ public class Main7569 {
                 }
             }
         }
-        return day;
+        int day = 0;
+        for (int i = 0; i < floor; i++) {
+            for (int j = 0; j < height; j++) {
+                for (int k = 0; k < width; k++) {
+                    day = Math.max(day, box[i][j][k]);
+                }
+            }
+        }
+        return day - 1;
     }
 
     public static void main(String[] args) throws IOException {
