ae4dab809d65849bba9d487dc2535fe44ee4e459
LEE <dulfla_5@naver.com>
2022-02-03 16:59:49 +0900

    A-B - success

---
+++ src/main/java/Main1001.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main1001 {
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void solution() throws IOException {
+        int[] operands = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(operands[0] - operands[1]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
