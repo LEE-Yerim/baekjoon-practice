5004b36cf45d79acdb741e7768148c79bc9b82b5
LEE <dulfla_5@naver.com>
2022-02-03 16:58:21 +0900

    A+B - runtime error

---
+++ src/main/java/Main1000.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main1000 {
+    private Scanner scanner = new Scanner(System.in);
+    public void addition() {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
