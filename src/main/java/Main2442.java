f843345a684c1877736944f37492970bb5e1b906
LEE <dulfla_5@naver.com>
2022-02-04 22:18:50 +0900

    별찍기5 - 출력 형식이 잘못되었습니다

---
+++ src/main/java/Main2442.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2442 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
