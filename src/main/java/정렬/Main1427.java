02fb3da377beadd51d43f5104e85ac99c440d37c
LEE <dulfla_5@naver.com>
2022-02-07 11:30:15 +0900

    소트인사이드 - update

--- src/main/java/정렬/Main1427.java
+++ src/main/java/정렬/Main1427.java
@@ -1,11 +1,14 @@
 package 정렬;
 
+// import java.io.*;
+// import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 
+//소트인사이드
 public class Main1427 {
     private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
