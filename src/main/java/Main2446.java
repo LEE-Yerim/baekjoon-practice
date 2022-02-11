4d89f2bba65e37c9cf9958432a7e281c7418ce42
LEE <dulfla_5@naver.com>
2022-02-11 12:43:29 +0900

    별찍기9 - success

---
+++ src/main/java/Main2446.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//별찍기9
+public class Main2446 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
