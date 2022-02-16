1619e4da98e826de3c227d53e2d376cc40056731
LEE <dulfla_5@naver.com>
2022-02-16 18:52:17 +0900

    기찍N - success

---
+++ src/main/java/Main2742.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+//기찍N
+public class Main2742 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
