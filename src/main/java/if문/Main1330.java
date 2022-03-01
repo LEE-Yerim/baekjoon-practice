19960539b533977e3a34c41f9babe6cbaa440b32
LEE <dulfla_5@naver.com>
2022-03-01 12:15:12 +0900

    두 수 비교하기 - update

--- src/main/java/if문/Main1330.java
+++ src/main/java/if문/Main1330.java
@@ -1,10 +1,13 @@
 package if문;
 
+// import java.io.*;
+// import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+//두 수 비교하기
 public class Main1330 {
     private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
