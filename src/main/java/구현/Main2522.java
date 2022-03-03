63c791c8cd1e2e5100a49ce71543c185ac156677
LEE <dulfla_5@naver.com>
2022-03-03 21:01:54 +0900

    별찍기12 - update

--- src/main/java/구현/Main2522.java
+++ src/main/java/구현/Main2522.java
@@ -1,9 +1,11 @@
 package 구현;
 
+// import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//별찍기12
 public class Main2522 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
