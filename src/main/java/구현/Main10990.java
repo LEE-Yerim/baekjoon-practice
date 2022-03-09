4466eb52b39d030048cc101484af28aadc98d644
LEE <dulfla_5@naver.com>
2022-03-09 09:02:38 +0900

    별찍기15 - update

--- src/main/java/구현/Main10990.java
+++ src/main/java/구현/Main10990.java
@@ -1,9 +1,11 @@
 package 구현;
 
+// import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//별찍기15
 public class Main10990 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
