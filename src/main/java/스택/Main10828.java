645ec848ea4546859dc9ac89763466a83cc1f27f
LEE <dulfla_5@naver.com>
2022-03-09 09:02:23 +0900

    스택 - update

--- src/main/java/스택/Main10828.java
+++ src/main/java/스택/Main10828.java
@@ -1,10 +1,13 @@
 package 스택;
 
+// import java.io.*;
+// import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
+//스택
 public class Main10828 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
