aa1cbf134d0e77a76c9dd80f61721f65e5ccdc5d
LEE <dulfla_5@naver.com>
2022-03-11 10:59:02 +0900

    별찍기21 - update

--- src/main/java/구현/Main10996.java
+++ src/main/java/구현/Main10996.java
@@ -1,9 +1,11 @@
 package 구현;
 
+// import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//별찍기21
 public class Main10996 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
