aae03411aeb658b8cdb3f4bd72d43fef93cbfcee
LEE <dulfla_5@naver.com>
2022-03-13 15:26:17 +0900

    IOIOI - 시간 초과

--- src/main/java/Main5525.java
+++ src/main/java/Main5525.java
@@ -1,28 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
+// IOIOI
 public class Main5525 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void solution() throws IOException {
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        List<String> pn_pieces = new ArrayList<>();
-        pn_pieces.add("I");
-        int index = 1;
-        for (int i = 0; i < n; i++) {
-            pn_pieces.add("OI");
-            index += 2;
-        }
-        String pn = String.join("", pn_pieces);
+        String[] s = br.readLine().split("");
         int count = 0;
-        for (int i = 0; i < m - index + 1; i++) {
-            if (s.substring(i, i + index).equals(pn)) {
-                count++;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].equals("I")) {
+                boolean flag = true;
+                for (int j = 0; j < n / 2; j++) {
+                    if (s[i + j].equals("O") && s[i + j + 1].equals("I")) {
+                        flag = false;
+                    }
+                }
+                if (flag) {
+                    count++;
+                }
             }
         }
         System.out.println(count);
