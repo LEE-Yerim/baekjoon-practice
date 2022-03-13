879569bada15c2517319871454851ac7e0e49037
LEE <dulfla_5@naver.com>
2022-03-13 15:09:02 +0900

    연결 요소의 개수 - update

--- src/main/java/그래프_이론/Main11724_bfs.java
+++ src/main/java/그래프_이론/Main11724_bfs.java
@@ -1,10 +1,12 @@
 package 그래프_이론;
 
+// import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+//연결 요소의 개수
 public class Main11724_bfs {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final Map<Integer, List<Integer>> map = new HashMap<>();
