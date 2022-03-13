8cca51719b536fd5492e7f5f675145e15af70b72
LEE <dulfla_5@naver.com>
2022-03-13 15:27:29 +0900

    IOIOI - 틀렸습니다

--- src/main/java/Main5525.java
+++ src/main/java/Main5525.java
@@ -5,21 +5,17 @@ public class Main5525 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static int n;
+    public static String[] s;
+
     public static void solution() throws IOException {
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        String[] s = br.readLine().split("");
+        s = br.readLine().split("");
         int count = 0;
         for (int i = 0; i < s.length; i++) {
             if (s[i].equals("I")) {
-                boolean flag = true;
-                for (int j = 0; j < n / 2; j++) {
-                    if (!s[i + j].equals("O") || !s[i + j + 1].equals("I")) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) {
+                if (check(i)) {
                     count++;
                 }
             }
@@ -27,6 +23,15 @@ public class Main5525 {
         bw.write(Integer.toString(count));
         bw.flush();
         bw.close();
+    }
+
+    public static boolean check(int i) {
+        for (int j = 0; j < n / 2; j++) {
+            if (!s[i + j].equals("O") || !s[i + j + 1].equals("I")) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
