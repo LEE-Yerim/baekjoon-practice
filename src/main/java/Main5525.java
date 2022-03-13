66fbcb3a565192ecc9bce07daf76ed5cb288aa0a
LEE <dulfla_5@naver.com>
2022-03-13 15:25:22 +0900

    IOIOI - 50점

--- src/main/java/Main5525.java
+++ src/main/java/Main5525.java
@@ -1,12 +1,9 @@
-// import java.io.*;
-// import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-//IOIOI
 public class Main5525 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
