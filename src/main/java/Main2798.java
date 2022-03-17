96f0d8a08895832aa0ad66274dc78337f61ddaa4
LEE <dulfla_5@naver.com>
2022-03-17 14:44:47 +0900

    블랙잭 - 틀렸습니다

--- src/main/java/Main2798.java
+++ src/main/java/Main2798.java
@@ -1,11 +1,10 @@
-// import java.io.*;
-// import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+// 블랙잭
 public class Main2798 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
