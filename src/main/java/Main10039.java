14ba3150ca206e723b538a678754916e82091e42
LEE <dulfla_5@naver.com>
2022-03-20 15:10:42 +0900

    평균 점수 - success

--- src/main/java/Main10039.java
+++ src/main/java/Main10039.java
@@ -2,19 +2,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main1712 {
+// 평균 점수
+public class Main10039 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void solution() throws IOException {
-        String[] prices = br.readLine().split(" ");
-        int A = Integer.parseInt(prices[0]);
-        int B = Integer.parseInt(prices[1]);
-        int C = Integer.parseInt(prices[2]);
-        int i = 0;
-        while (A + B * i >= C * i) {
-            i++;
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int score = Integer.parseInt(br.readLine());
+            if (score < 40) {
+                score = 40;
+            }
+            sum += score;
         }
-        System.out.println(i);
+        System.out.println(sum / 5);
     }
 
     public static void main(String[] args) throws IOException {
