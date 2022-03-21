efe45e749f4a067c81d02867ba12fea6bc61ea76
LEE <dulfla_5@naver.com>
2022-03-21 14:57:10 +0900

    큰 수 A+B - success

---
+++ src/main/java/Main10757.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+// 큰 수 A+B
+public class Main10757 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String[] numbers = br.readLine().split(" ");
+        BigInteger sum = new BigInteger("0");
+        for (int i = 0; i < 2; i++) {
+            sum = sum.add(new BigInteger(numbers[i]));
+        }
+        System.out.println(sum);
+    }
+}
