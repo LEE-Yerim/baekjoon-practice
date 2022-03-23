8659e4235389ccd1660400d3c8d32c82c68d9400
LEE <dulfla_5@naver.com>
2022-03-23 14:12:06 +0900

    사칙연산 - success

---
+++ src/main/java/Main10869.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 사칙연산
+public class Main10869 {
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void solution() throws IOException {
+        int[] operands = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(operands[0] + operands[1]);
+        System.out.println(operands[0] - operands[1]);
+        System.out.println(operands[0] * operands[1]);
+        System.out.println(operands[0] / operands[1]);
+        System.out.println(operands[0] % operands[1]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
