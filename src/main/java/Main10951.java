aa9d43ba0110f7228101cc8e7926799080278543
LEE <dulfla_5@naver.com>
2022-03-24 12:53:36 +0900

    A+B 4 - success

--- src/main/java/Main10951.java
+++ src/main/java/Main10951.java
@@ -5,23 +5,22 @@ import java.util.Arrays;
 
 // A+B - 4
 public class Main10951 {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        while ((str = br.readLine()) != null) {
+            int[] numbers = Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
+            int sum = 0;
+            for (int number : numbers) {
+                sum += number;
+            }
+            System.out.println(sum);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        boolean flag = true;
-        while (flag) {
-            String input = br.readLine();
-            if (!input.isBlank()) {
-                int[] numbers = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
-                int sum = 0;
-                for(int number : numbers) {
-                    sum += number;
-                }
-                System.out.println(sum);
-            }
-            if (input.isBlank()) {
-                flag = false;
-            }
-        }
+        solution();
     }
 }
