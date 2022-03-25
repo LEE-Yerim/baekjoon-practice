a5783dd940c1457f22d37f8f631d1cfe28776c37
LEE <dulfla_5@naver.com>
2022-03-25 11:57:18 +0900

    A+B 7 - success

---
+++ src/main/java/Main11021.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// A+B - 7
+public class Main11021 {
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void solution() throws IOException {
+        int t = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < t; i++) {
+            int[] operands = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            System.out.println("Case #" + (i + 1) + ": " + (operands[0] + operands[1]));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
