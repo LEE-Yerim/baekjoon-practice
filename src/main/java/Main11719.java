410d7e21b20f47964f7fabff27bf3f565330353d
LEE <dulfla_5@naver.com>
2022-03-28 15:53:17 +0900

    그대로 출력하기 2 - success

---
+++ src/main/java/Main11719.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 그대로 출력하기 2
+public class Main11719 {
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void solution() throws IOException {
+        try
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String str = "";
+
+            while((str = br.readLine()) != null)
+            {
+                System.out.println(str);
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
