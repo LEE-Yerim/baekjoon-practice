423db754e8268eaa5d3e49a8906f55f3d956dced
LEE <dulfla_5@naver.com>
2022-03-28 15:53:42 +0900

    숫자의 합 - success

--- src/main/java/Main11720.java
+++ src/main/java/Main11720.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // 숫자의 합
 public class Main11720 {
-    public static void solution() {
-        Scanner scanner1 = new Scanner(System.in);
-        int size = scanner1.nextInt();
-        Scanner scanner2 = new Scanner(System.in);
-        String numbers = scanner2.nextLine();
+    private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static void solution() throws IOException {
+        int size = Integer.parseInt(bf.readLine());
+        String numbers = bf.readLine();
 
         int sum = 0;
         for (String number : numbers.split("")) {
@@ -16,7 +17,7 @@ public class Main11720 {
         System.out.println(sum);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         solution();
     }
 }
